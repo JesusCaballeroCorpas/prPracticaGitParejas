fbab54d3d3091d61f882807e1faab4e96b750dd0
JesusCaballero <jesuscaba84@gmail.com>
2016-11-14 18:05:33 +0100

    Primer commit

---
+++ src/prPracticaGitParejas/GitAux.java
@@ -0,0 +1,12 @@
+package prPracticaGitParejas;
+
+public class GitAux {
+	public void metodoAlumno1(){
+		System.out.println("metodo1");
+	}
+	
+	public void metodoComunitario(){
+		//Comentario incluido para pruebas
+		System.out.println("Aqui escribimos todos");
+	}
+}
