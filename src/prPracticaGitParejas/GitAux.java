afdcb0c388f978921a7d2c3a577ab561f839514d
JesusCaballero <jesuscaba84@gmail.com>
2016-11-14 18:26:16 +0100

    Se resuleven los conflictos

--- src/prPracticaGitParejas/GitAux.java
+++ src/prPracticaGitParejas/GitAux.java
@@ -9,14 +9,11 @@ public class GitAux {
 	public void metodoComunitario(){
 		//Comentario incluido para pruebas
 		System.out.println("Aqui escribimos todos");
-<<<<<<< HEAD
-	System.out.println("alumno 2"); // modif alumno 2
+		System.out.println("alumno 2"); // modif alumno 2
+		System.out.println("alumno 1 inserta esta sentencia");
 	}
 	
 	public void metodoAlumno2(){
 		System.out.println("Metodo 2");
-=======
-		System.out.println("alumno 1 inserta esta sentencia");
->>>>>>> 17701851cb3250c9f3b7ea6990c845d2c5793fa9
 	}
 }
