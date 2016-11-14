17701851cb3250c9f3b7ea6990c845d2c5793fa9
JesusCaballero <jesuscaba84@gmail.com>
2016-11-14 18:11:41 +0100

    Alumno 1 ha modificado el archivo

--- src/prPracticaGitParejas/GitAux.java
+++ src/prPracticaGitParejas/GitAux.java
@@ -3,10 +3,12 @@ package prPracticaGitParejas;
 public class GitAux {
 	public void metodoAlumno1(){
 		System.out.println("metodo1");
+		System.out.println("modificado por alum 1");
 	}
 	
 	public void metodoComunitario(){
 		//Comentario incluido para pruebas
 		System.out.println("Aqui escribimos todos");
+		System.out.println("alumno 1 inserta esta sentencia");
 	}
 }
